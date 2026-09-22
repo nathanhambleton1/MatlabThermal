2ce4df142895b31200852b2ae12a1c143692b8f0
Nathan Hambleton <nhambleton03@gmail.com>
2026-09-22 09:13:51 -0400

    abc

---
+++ generated/batt21700_param.m
@@ -0,0 +1,25 @@
+%% Battery parameters
+
+%% ModuleType1
+ModuleType1.SOC_vecCell = [0, .1, .25, .5, .75, .9, 1]; % Vector of state-of-charge values, SOC
+ModuleType1.T_vecCell = [278, 293, 313]; % Vector of temperatures, T, K
+ModuleType1.V0_matCell = [3.49, 3.5, 3.51; 3.55, 3.57, 3.56; 3.62, 3.63, 3.64; 3.71, 3.71, 3.72; 3.91, 3.93, 3.94; 4.07, 4.08, 4.08; 4.19, 4.19, 4.19]; % Open-circuit voltage, V0(SOC,T), V
+ModuleType1.V_rangeCell = [0, inf]; % Terminal voltage operating range [Min Max], V
+ModuleType1.R0_matCell = [.0117, .0085, .009; .011, .0085, .009; .0114, .0087, .0092; .0107, .0082, .0088; .0107, .0083, .0091; .0113, .0085, .0089; .0116, .0085, .0089]; % Terminal resistance, R0(SOC,T), Ohm
+ModuleType1.AHCell = 27; % Cell capacity, AH, A*hr
+ModuleType1.thermal_massCell = 100; % Thermal mass, J/K
+ModuleType1.AmbientResistance = 25; % Cell level ambient thermal path resistance, K/W
+ModuleType1.XminThermalResistance = 25; % Cell-level thermal path resistance at Xmin boundary, K/W
+ModuleType1.XmaxThermalResistance = 25; % Cell-level thermal path resistance at Xmax boundary, K/W
+
+%% ParallelAssemblyType1
+ParallelAssemblyType1.SOC_vecCell = [0, .1, .25, .5, .75, .9, 1]; % Vector of state-of-charge values, SOC
+ParallelAssemblyType1.T_vecCell = [278, 293, 313]; % Vector of temperatures, T, K
+ParallelAssemblyType1.V0_matCell = [3.49, 3.5, 3.51; 3.55, 3.57, 3.56; 3.62, 3.63, 3.64; 3.71, 3.71, 3.72; 3.91, 3.93, 3.94; 4.07, 4.08, 4.08; 4.19, 4.19, 4.19]; % Open-circuit voltage, V0(SOC,T), V
+ParallelAssemblyType1.V_rangeCell = [0, inf]; % Terminal voltage operating range [Min Max], V
+ParallelAssemblyType1.R0_matCell = [.0117, .0085, .009; .011, .0085, .009; .0114, .0087, .0092; .0107, .0082, .0088; .0107, .0083, .0091; .0113, .0085, .0089; .0116, .0085, .0089]; % Terminal resistance, R0(SOC,T), Ohm
+ParallelAssemblyType1.AHCell = 27; % Cell capacity, AH, A*hr
+ParallelAssemblyType1.thermal_massCell = 100; % Thermal mass, J/K
+ParallelAssemblyType1.AmbientResistance = 25; % Cell level ambient thermal path resistance, K/W
+ParallelAssemblyType1.XminThermalResistance = 25; % Cell-level thermal path resistance at Xmin boundary, K/W
+ParallelAssemblyType1.XmaxThermalResistance = 25; % Cell-level thermal path resistance at Xmax boundary, K/W
